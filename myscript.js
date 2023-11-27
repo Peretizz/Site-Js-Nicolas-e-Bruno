adfaa0b429638ab8a7e90d2d8d672749edf8086d
Peretizz <143546401+Peretizz@users.noreply.github.com>
2023-11-27 19:46:21 +0000

    as

--- myscript.js
+++ myscript.js
@@ -1,71 +1,94 @@
-function validateAndSubmit() {
+document.addEventListener('DOMContentLoaded', function () {
+    // Array para armazenar os itens do carrinho
+    var carrinhoItens = [];
 
-    let name = document.getElementById("name").value;
-    let email = document.getElementById("email").value;
-    let cpf = document.getElementById("cpf").value;
-    let phone = document.getElementById("phone").value;
-    let cep = document.getElementById("cep").value;
+    // Função para atualizar o carrinho e calcular o total
+    function atualizarCarrinho() {
+        // Lógica para atualizar o carrinho aqui
+        // ...
 
-    let products = [
-        { name: "Product 1", price: 50, quantity: 2 },
-        { name: "Product 2", price: 30, quantity: 1 },
+        // Atualize o total
+        var total = carrinhoItens.reduce((acc, item) => acc + item.preco * item.quantidade, 0);
+        document.getElementById('total').textContent = 'Total: R$ ' + total.toFixed(2);
+    }
 
-    ];
+    // Função para adicionar um item ao carrinho
+    function adicionarAoCarrinho(nome, preco, quantidade) {
+        carrinhoItens.push({ nome: nome, preco: preco, quantidade: quantidade });
+        atualizarCarrinho();
+    }
 
+    // Função para remover um item do carrinho
+    function removerDoCarrinho(index) {
+        carrinhoItens.splice(index, 1);
+        atualizarCarrinho();
+    }
 
-    let totalSale = products.reduce((total, product) => total + product.price * product.quantity, 0);
-
-    let previousTotalSales = localStorage.getItem('totalSales');
-
-
-    let totalSalesArray = previousTotalSales ? JSON.parse(previousTotalSales) : [];
-
-    totalSalesArray.push(totalSale);
-
-    localStorage.setItem('totalSales', JSON.stringify(totalSalesArray));
-
-    console.log("Order Details:", { name, email, cpf, phone, cep, products, totalSale });
-
-    fetch('https://cors-anywhere.herokuapp.com/https://app.headlessforms.cloud/api/v1/form-submission/WF0tuafB2d', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ name, email, cpf, phone, cep, products, totalSale }),
-    })
-    .then(response => response.json())
-    .then(data => {
-        console.log('Success:', data);
-
-    })
-    .catch((error) => {
-        console.error('Error:', error);
-
+    // Evento de clique nos botões de compra
+    var botoesCompra = document.querySelectorAll('.card button');
+    botoesCompra.forEach(function (botao, index) {
+        botao.addEventListener('click', function () {
+            // Substitua esses valores pelos dados reais do produto
+            var nome = 'Nome do Produto';
+            var preco = 19.99;
+            var quantidade = 1;
+            adicionarAoCarrinho(nome, preco, quantidade);
+        });
     });
 
-    emailjs.send("default_service", "your_emailjs_template_id", {
-        name: name,
-        email: email,
+    // Evento de clique no botão de finalizar compra
+    document.getElementById('finalizarCompra').addEventListener('click', function () {
+        // Lógica para validar o formulário aqui
+        // ...
 
-    })
-    .then(function(response) {
-        console.log("Email sent successfully:", response);
-    }, function(error) {
-        console.log("Email failed to send:", error);
+        // Lógica para enviar os dados do formulário via e-mail
+        // ...
+
+        // Lógica para salvar o total da venda no localStorage
+        localStorage.setItem('historicoVendas', JSON.stringify(carrinhoItens.map(item => item.preco * item.quantidade)));
+
+        // Lógica para enviar os dados ao servidor
+        // ...
+
+        // Lógica para mostrar o total das vendas no site
+        var historicoVendas = JSON.parse(localStorage.getItem('historicoVendas'));
+        var totalVendas = historicoVendas ? historicoVendas.reduce((acc, venda) => acc + venda, 0) : 0;
+        document.getElementById('totalVendas').textContent = 'Total de Vendas: R$ ' + totalVendas.toFixed(2);
     });
 
+    // Evento de clique nos botões de remoção do carrinho
+    var botoesRemover = document.querySelectorAll('.remover');
+    botoesRemover.forEach(function (botao, index) {
+        botao.addEventListener('click', function () {
+            removerDoCarrinho(index);
+        });
+    });
+})
+var cartItems = [];
+var cartTotal = 0;
 
+function addToCart(productName, price) {
+  cartItems.push({ name: productName, price: price });
+  cartTotal += price;
+
+  // Atualizar o carrinho de compras na página
+  updateCart();
 }
 
-function calculateTotalSales() {
+function updateCart() {
+  var cartItemsList = document.getElementById('cart-items');
+  var cartTotalElement = document.getElementById('cart-total');
 
-    let totalSalesArray = localStorage.getItem('totalSales');
-   
+  // Limpar a lista de itens do carrinho
+  cartItemsList.innerHTML = '';
 
-    totalSalesArray = totalSalesArray ? JSON.parse(totalSalesArray) : [];
+  // Adicionar cada item ao carrinho à lista
+  cartItems.forEach(function(item) {
+    var listItem = document.createElement('li');
+    listItem.textContent = item.name + ' - R$' + item.price;
+    cartItemsList.appendChild(listItem);
+  });
 
-    let totalSales = totalSalesArray.reduce((total, sale) => total + sale, 0);
-
-
-    document.getElementById('totalSales').innerText = `Total de Vendas: ${totalSales}`;
+  // Atualizar o total do carrinho
+  cartTotalElement.textContent = cartTotal.toFixed(2);
 }
